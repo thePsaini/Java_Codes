d1d907530532e9112e28e6bf0d172ae2be779cb1
Priyanshu Saini <the.p.saini@gmail.com>
2026-04-30 16:08:39 +0530

    added java program

---
+++ bitbybit.java
@@ -0,0 +1,12 @@
+ public class bitbybit {
+    public static void main(String[] args) {
+        int a=9, b=10;
+    System.out.println(a|b);
+     System.out.println(a&b);
+      System.out.println(a^b);
+      System.out.println(a<<1);
+      System.out.println(a<<2);
+      System.out.println(b>>1);
+      System.out.println(b>>2);
+    }
+}
